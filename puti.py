08de63bdb12c8f19692f4d4bc791edff4abd69dc
Александр <aashchukarev@yandex.ru>
2021-05-01 22:59:10 +0300

    Записываем пути в которых есть файлы с расширением.py

---
+++ puti.py
@@ -0,0 +1,12 @@
+import os.path
+
+spisok=[]
+for current_dir, dirs, files in os.walk('\main'):
+    for elem in files:
+        if elem.find('.py') > 0:
+            spisok.append(current_dir[1:])
+            break
+
+content = '\n'.join(sorted(spisok))
+with open('C:\stepik\dataset_3333_otv.txt', 'w') as wile:
+    wile.write(content)
