2ba86512e042d0e64347a3302355a20209606c6d
Александр <aashchukarev@yandex.ru>
2021-05-16 18:21:54 +0300

    Добавил шаблон даты по 2015 году, отобрал все преступления за этот период и посчитал их, результат записал в словарь (ключ - преступление, значение - кол-во происшествий). Вывел самое часто встречающееся.

--- csv341.py
+++ csv341.py
@@ -3,12 +3,21 @@
 # Одним из атрибутов преступления является его тип – Primary Type.
 # Вам необходимо узнать тип преступления, которое было зафиксировано максимальное число раз в 2015 году.
 import csv
+import re
 
+crimes_2015 = {}
+pat_rn = re.compile(r"\d\d\/\d\d\/2015")
 with open('C:\stepik\Crimes.csv', 'r') as f:
     file = csv.DictReader(f)
     for row in file:
-        print(row)
-
+        if pat_rn.search(row['Date']):
+            if row['Primary Type'] not in crimes_2015:
+                crimes_2015[row['Primary Type']] = 1
+            else:
+                crimes_2015[row['Primary Type']] += 1
+max_crime_count = max(crimes_2015.values())
+most_crime = [k for k, v in crimes_2015.items() if v == max_crime_count]
+print(*most_crime)
 
 
 
